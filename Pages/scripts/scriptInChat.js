ba0be269df5ad74aac209bbee35cad55609b5eb5
Герман Кульчицкий <g.kulczycki@yandex.ru>
2019-07-23 14:42:22 +0300

    Удалена папка Pages

--- Pages/scripts/scriptInChat.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-bttn.onclick = () => {
-    const text = message.value;
-    let newMessage = document.createElement("div");
-    newMessage.innerHTML = text;
-    document.getElementById("main").appendChild(newMessage);
-}

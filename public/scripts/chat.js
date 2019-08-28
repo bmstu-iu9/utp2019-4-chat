cd257722c4cf048eef6cf0668c68b196cd5dc0d8
Roman Kovayin <kovajkin2012@yandex.ru>
2019-08-29 00:08:36 +0300

    added rendering new added pages

---
+++ public/scripts/chat.js
@@ -0,0 +1,7 @@
+"use strict";
+bttn.onclick = () => {
+	const text = message.value;
+	let newMessage = document.createElement("div");
+	newMessage.innerHTML = text;
+	document.getElementById("main").appendChild(newMessage);
+};

7fb1244fa963f0d288f1fdab5be742be263b9ff6
jetsnake <g.kulczycki@yandex.ru>
2019-08-30 14:38:53 +0300

    Отправка сообщений

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,7 +1,21 @@
 "use strict";
 bttn.onclick = () => {
 	const text = message.value;
-	let newMessage = document.createElement("div");
-	newMessage.innerHTML = text;
-	document.getElementById("main").appendChild(newMessage);
+	if (text) {
+		let newMessage = document.createElement("div");
+		newMessage.setAttribute("id", "my-message-box");
+		
+		let picture = document.createElement("img");
+		picture.setAttribute("id", "m-img");
+		picture.setAttribute("src", "../public/usericon2.jpg");
+		picture.setAttribute("alt", "Беда!");
+		newMessage.appendChild(picture);
+		
+		let messageText = document.createElement("p");
+		messageText.setAttribute("id", "m-text");
+		messageText.innerText = text;
+		newMessage.appendChild(messageText);
+		message.value = "";
+		document.getElementById("main").appendChild(newMessage);
+	}
 };
--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.use((req, res, next)=> {
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
